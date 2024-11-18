c878680cdd2905f65a3abec1a69ed524328efe9d
Abubeker Mohammed <abumoha1892@gmail.com>
2024-11-18 11:18:46 +0300

    initial commit

--- images/index.ts
+++ images/index.ts
@@ -1,24 +1,8 @@
-// images/index.ts
+import sparkles from "./sparkles.webp";
+import trophy from "./trophy.webp";
+import paws from "./fuas.jpg";
+import taskWhitePaws from "./task-white-paws.webp";
+import taskBoost from "./task-boost.webp";
+import taskBlum from "./task-blum.webp";
 
-/**
- * This project was developed by Nikandr Surkov.
- * 
- * YouTube: https://www.youtube.com/@NikandrSurkov
- * GitHub: https://github.com/nikandr-surkov
- */
-
-import sparkles from "./sparkles.webp"
-import trophy from "./trophy.webp"
-import paws from "./paws.webp"
-import taskWhitePaws from "./task-white-paws.webp"
-import taskBoost from "./task-boost.webp"
-import taskBlum from "./task-blum.webp"
-
-export {
-    sparkles,
-    trophy,
-    paws,
-    taskWhitePaws,
-    taskBoost,
-    taskBlum
-}
+export { sparkles, trophy, paws, taskWhitePaws, taskBoost, taskBlum };
